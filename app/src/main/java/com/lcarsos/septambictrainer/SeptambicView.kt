05ebeb7eb6d9425e1b8b77afa100cfb3169904cc
Ezekiel Chopper <lcarsos@lcarsos.com>
2021-03-04 19:09:05 -0700

    Adding SeptambicView

---
+++ app/src/main/java/com/lcarsos/septambictrainer/SeptambicView.kt
@@ -0,0 +1,11 @@
+package com.lcarsos.septambictrainer
+
+import android.graphics.Canvas
+import android.view.View
+
+class SeptambicView: View() {
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+    }
+}
