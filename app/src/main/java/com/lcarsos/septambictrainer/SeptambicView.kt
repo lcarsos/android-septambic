8c62b87d7340fef9bec5898ff11ff62dbe4e6570
Ezekiel Chopper <lcarsos@lcarsos.com>
2021-03-06 17:38:05 -0700

    figuring out Kotlin class declarations

--- app/src/main/java/com/lcarsos/septambictrainer/SeptambicView.kt
+++ app/src/main/java/com/lcarsos/septambictrainer/SeptambicView.kt
@@ -1,9 +1,12 @@
 package com.lcarsos.septambictrainer
 
+import android.content.Context
 import android.graphics.Canvas
+import android.util.AttributeSet
 import android.view.View
 
-class SeptambicView: View() {
+class SeptambicView(context: Context, attrs: AttributeSet): View(context, attrs) {
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
